f8b8c3a329bf5ef09e686d6d46a39cd75a8a7273
yazi <mohammedyasin1512@gmail,com>
2023-05-26 08:13:55 +0530

    all images added

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-const products = [{
-    img: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    price: 1090
-}, {
-    img: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    price: 2095
-}, {
-    img: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    price: 799
-}];
+
 
 let productHtml = ''
 
@@ -32,7 +8,7 @@ products.forEach((product) => {
     <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${product.img}">
+              src="${product.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
@@ -48,7 +24,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            ${(product.price / 100).toFixed(2)}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -86,7 +62,7 @@ products.forEach((product) => {
     
      
 });
-console.log(productHtml);
+
 
 document.querySelector('.js-products-grids')
  .innerHTML = productHtml;
