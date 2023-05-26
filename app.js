5b766a7532633155fba4d3a109ff47e8a4fedbb1
yazi <mohammedyasin1512@gmail,com>
2023-05-26 23:00:36 +0530

    added cart btn

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 
-
 let productHtml = ''
 
 products.forEach((product) => {
@@ -49,7 +48,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-added" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -66,3 +65,38 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grids')
  .innerHTML = productHtml;
+
+ document.querySelectorAll('.js-added').forEach((button) => {
+    button.addEventListener('click',() => {
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if( productId === item.productId){
+
+          matchingItem = item;
+        }
+
+      });
+
+      if (matchingItem){
+
+        matchingItem.quantity += 1;
+
+      }else {
+
+        cart.push({
+          productId,
+          quantity:1
+        });
+
+      };
+
+      console.log(cart)
+      
+      
+    });
+    
+    
+ });
